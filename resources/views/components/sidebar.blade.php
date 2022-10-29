26f446fcafabd2383504d03ab4b92be576d12c94
YStorm5 <panhagt5@gmail.com>
2022-10-29 23:54:47 +0700

    add link to signin and signup page

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -80,7 +80,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="#"
+                        <a href="/login"
                             class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
                             <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
                                 fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -92,7 +92,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="#"
+                        <a href="/signup"
                             class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
                             <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
                                 fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -150,7 +150,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="#"
+                        <a href="/login"
                             class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
                             <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
                                 fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -162,7 +162,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="#"
+                        <a href="/signup"
                             class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
                             <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
                                 fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -198,7 +198,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="#"
+                        <a href="/login"
                             class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
                             <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
                                 fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -210,7 +210,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="#"
+                        <a href="/signup"
                             class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
                             <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
                                 fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
